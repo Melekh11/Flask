2d98765e9c0a4410267d606b9d4e7f205cb6d6ac
Матвей <cktkutxd@gmail.com>
2022-03-01 10:40:52 +0300

    Загрузка файла

--- main.py
+++ main.py
@@ -1,42 +1,74 @@
 # c днём рождения Вас!!)
 
-from flask import Flask, url_for
+from flask import Flask, url_for, request
+from PIL import Image
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "<h1>Привет, колонизатор</h1>"
+@app.route('/', methods=['POST', 'GET'])
+def image_mars():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save("static/img/pic.png")
+        im = Image.open("static/img/pic.png")
+        im2 = im.resize((430, 430))
+        im2.save('static/img/pic.png')
 
+        return f'''<!doctype html>
+                                        <html lang="en">
+                                          <head>
+                                            <meta charset="utf-8">
+                                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                            <link rel="stylesheet"
+                                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                            crossorigin="anonymous">
+                                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                            <title>Пример формы</title>
+                                          </head>
+                                          <body>
+                                            <div>
+                                                <form class="login_form" method="post" enctype="multipart/form-data">
+                                                    <label>Загрузите Ваше фото</label>
+                                                    <div class="form-group">
+                                                        <label for="photo">Приложите фотографию</label>
+                                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                                    </div>   
+                                                    <img src="{url_for('static', filename='img/pic.png')}"
+                                                        alt="здесь должна была быть картинка, но не нашлась">
+                                                     <button type="submit" class="btn btn-primary">Показать</button>
+                                                </form>
+                                            </div>
+                                          </body>
+                                        </html>'''
+    elif request.method == 'GET':
+        return f'''<!doctype html>
+                                <html lang="en">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                    <link rel="stylesheet"
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                    crossorigin="anonymous">
+                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                    <title>Пример формы</title>
+                                  </head>
+                                  <body>
+                                    <div>
+                                        <form class="login_form" method="post" enctype="multipart/form-data">
+                                            <label>Загрузите Ваше фото</label>
+                                            <div class="form-group">
+                                                <label for="photo">Приложите фотографию</label>
+                                                <input type="file" class="form-control-file" id="photo" name="file">
+                                            </div>   
+                                             <button type="submit" class="btn btn-primary">Показать</button>
+                                        </form>
+                                    </div>
+                                  </body>
+                                </html>'''
 
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def image_mars(nickname, level, rating):
-    return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <title>Выбор планеты</title>
-                          </head>
-                          <body>
-                            <div class="alert alert-primary" role="alert">
-                              <h1>Добрый день, {nickname}</h1>
-                            </div>
-                            <div class="alert alert-secondary" role="alert">
-                              <h2>На данный момент Ваш этап: {level}</h2>
-                            </div>
-                            <div class="alert alert-success" role="alert">
-                              <h3>а рейтинг: {rating}</h3>
-                            </div>
-                            <div class="alert alert-danger" role="alert">
-                              <h4>all the best of luck to you, have a nice day</h4>
-                            </div>
-                          </body>
-                        </html>'''
 
 
 if __name__ == '__main__':
