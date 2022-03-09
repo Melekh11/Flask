d53f33119c7e1c024cf980ffdf5182c8c4c0a113
Матвей <cktkutxd@gmail.com>
2022-03-09 18:35:30 +0300

    Изображение Марса 2.0

--- main.py
+++ main.py
@@ -1,74 +1,34 @@
-# c днём рождения Вас!!)
-
-from flask import Flask, url_for, request
-from PIL import Image
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
+def index():
+    return "<h1>Миссия Колонизация Марса</h1>"
+
+
+@app.route('/index')
+def index1():
+    return "<h1>И на Марсе будут яблони цвести!</h1>"
+
+
+@app.route("/promotion")
+def promotion():
+    return"<h2>Человечество вырастает из детства.</h2>" \
+          "<h2>Человечеству мала одна планета.</h2>" \
+          "<h2>Мы сделаем обитаемыми безжизненные пока планеты.</h2>" \
+          "<h2>И начнем с Марса!</h2>" \
+          "<h2>Присоединяйся!</h2>"
+
+
+@app.route("/image_mars")
 def image_mars():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save("static/img/pic.png")
-        im = Image.open("static/img/pic.png")
-        im2 = im.resize((430, 430))
-        im2.save('static/img/pic.png')
-
-        return f'''<!doctype html>
-                                        <html lang="en">
-                                          <head>
-                                            <meta charset="utf-8">
-                                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                            <link rel="stylesheet"
-                                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                            crossorigin="anonymous">
-                                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                                            <title>Пример формы</title>
-                                          </head>
-                                          <body>
-                                            <div>
-                                                <form class="login_form" method="post" enctype="multipart/form-data">
-                                                    <label>Загрузите Ваше фото</label>
-                                                    <div class="form-group">
-                                                        <label for="photo">Приложите фотографию</label>
-                                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                                    </div>   
-                                                    <img src="{url_for('static', filename='img/pic.png')}"
-                                                        alt="здесь должна была быть картинка, но не нашлась">
-                                                     <button type="submit" class="btn btn-primary">Показать</button>
-                                                </form>
-                                            </div>
-                                          </body>
-                                        </html>'''
-    elif request.method == 'GET':
-        return f'''<!doctype html>
-                                <html lang="en">
-                                  <head>
-                                    <meta charset="utf-8">
-                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                    <link rel="stylesheet"
-                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                    crossorigin="anonymous">
-                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                                    <title>Пример формы</title>
-                                  </head>
-                                  <body>
-                                    <div>
-                                        <form class="login_form" method="post" enctype="multipart/form-data">
-                                            <label>Загрузите Ваше фото</label>
-                                            <div class="form-group">
-                                                <label for="photo">Приложите фотографию</label>
-                                                <input type="file" class="form-control-file" id="photo" name="file">
-                                            </div>   
-                                             <button type="submit" class="btn btn-primary">Показать</button>
-                                        </form>
-                                    </div>
-                                  </body>
-                                </html>'''
-
+    return f'''<title>Привет, Яндекс!</title>
+        <h1>Жди нас, Марс!</h1>
+        <img src="{url_for('static', filename='img/scale_1200.jpeg')}"
+           alt="здесь должна была быть картинка, но не нашлась">
+           <p>красиво...</p>'''
 
 
 if __name__ == '__main__':
