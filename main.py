6fc6da0dc5fd2ad6151cf4d81a9e5531d4166c3c
Матвей <cktkutxd@gmail.com>
2022-03-10 13:45:24 +0300

    Готовимся к миссии 2.0

--- main.py
+++ main.py
@@ -12,25 +12,6 @@ def index():
     return render_template('index.html', **all)
 
 
-@app.route("/training/<prof>")
-def training(prof):
-    all = {}
-    all["title"] = "тайтл"
-    all["pro"] = prof
-    all["first"] = url_for('static', filename='img/rover1.png')
-    all["second"] = url_for('static', filename='img/rover2.png')
-    return render_template("index.html", **all)
-
-
-@app.route("/list_prof/<smt>")
-def list_prof(smt):
-    l = ["строитель", "электрик", "врач", "оператор", "штурман", "пилот", "метеоролог", "экзобиолог"]
-    al = {}
-    al["l"] = l
-    al["smt"] = smt
-    return render_template("index.html", **al)
-
-
 # @app.route('/login', methods=['GET', 'POST'])
 # def login():
 #     form = LoginForm()
