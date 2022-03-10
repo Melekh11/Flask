28c166f393e2fa4d1519dc5be8268a5c485b0c65
Матвей <cktkutxd@gmail.com>
2022-03-10 23:53:37 +0300

    Изображение марса 105.0

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-from flask import Flask, url_for, render_template
-# from loginform import LoginForm
+from flask import Flask, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-# a = url_for('static', filename='img/scale_1200.jpeg')
-# print(a)
+@app.route('/')
+def index():
+    return "<h1>Миссия Колонизация Марса</h1>"
 
 
-@app.route('/index/<tit>')
-def index(tit):
-    all = {}
-    all["title"] = tit
-    return render_template('index.html', **all)
+@app.route('/index')
+def index1():
+    return "<h1>И на Марсе будут яблони цвести!</h1>"
 
 
-@app.route("/")
-@app.route("/training/<prof>")
-def training(prof):
-    all = {}
-    all["prof"] = prof
-    all["link1"] = url_for('static', filename='img/rover1.png')
-    all["link2"] = url_for('static', filename='img/rover2.png')
-    print("!!!!!!!")
-    print(url_for('static', filename='img/scale_1200.jpeg'))
-    return render_template("index.html", **all)
+@app.route("/promotion")
+def promotion():
+    return"<h2>Человечество вырастает из детства.</h2>" \
+          "<h2>Человечеству мала одна планета.</h2>" \
+          "<h2>Мы сделаем обитаемыми безжизненные пока планеты.</h2>" \
+          "<h2>И начнем с Марса!</h2>" \
+          "<h2>Присоединяйся!</h2>"
 
 
+@app.route("/image_mars")
+def image_mars():
+    return f'''<!doctype html>
+                                    <html lang="en">
+                                      <head>
+                                        <title>Привет, Яндекс!</title>
+                                      </head>
+                                      <body>
+                                        <img src="{url_for('static', filename='img/scale_1200.jpeg')}"
+                                           alt="здесь должна была быть картинка, но не нашлась">
+                                           <p>красиво...</p>
+                                      </body>
+                                    </html>'''
 
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         return redirect('/success')
-#     return render_template('login.html', title='Авторизация', form=form)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
