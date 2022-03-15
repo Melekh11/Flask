09e41f51d1c54fe501fbbd9bd96830fa03175c6d
Матвей <cktkutxd@gmail.com>
2022-03-15 18:37:05 +0300

    Автоматический ответ

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, url_for, request,render_template
 from PIL import Image
 
@@ -6,7 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def image_mars():
-    return render_template("index1.html")
+    return render_template("index.html")
+
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def ans():
+    all= {}
+    with open("ans.json", "r", encoding="utf-8") as file:
+        all["ans"] = json.load(file)
+    return render_template("index.html", **all)
 
 
 if __name__ == '__main__':
